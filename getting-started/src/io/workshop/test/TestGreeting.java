01b3ab96591133c283a16f27382d250bc875f326
vinodhmahendra <vinodh.mahendra@gmail.com>
2020-06-06 14:34:39 +0000

    created TestGreeting.java file

---
+++ getting-started/src/io/workshop/test/TestGreeting.java
@@ -0,0 +1,13 @@
+package io.workshop.test;
+
+import io.workshop.domain.Greeting;
+
+public class TestGreeting {
+
+	public static void main(String[] args) {
+		Greeting hello = new Greeting();
+		hello.greet();
+
+	}
+
+}
