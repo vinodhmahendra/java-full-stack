c4dcd0108eb33c8171ca42137088aa0376fafe5d
vinodhmahendra <vinodh.mahendra@gmail.com>
2020-06-06 15:54:01 +0000

    declared a constructor in Dog.java

--- object-oriented/src/io/workshop/domain/Dog.java
+++ object-oriented/src/io/workshop/domain/Dog.java
@@ -6,6 +6,11 @@ public class Dog {
 	// <modifier>* type name [ = <initial_value>];
 	private int weight;
 	
+	
+	public Dog() {
+		weight = 42;
+	}
+	
 	//syntax declaring methods
 	// <modifier>* <return-type> name( <argument> *) {
 	// <statement> *
